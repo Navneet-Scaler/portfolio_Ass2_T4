13c1dca93a859e89745bcdbfcd3055b947d97b11
Navneet-Scaler <navneet.23bcs10136@ms.sst.scaler.com>
2024-06-02 23:32:41 +0530

    Files Added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import About from "./components/about/about";
+import Intro from "./components/intro/intro";
+
+const App = () => {
+
+    return (
+        <div>
+            <Intro />
+            {/* <About /> */}
+        </div>
+    );
+};
+  
+export default App;
